ed6701f709a976a0f9ba53dee7c24ef235690b18
LeThiHoa04 <129196168+LeThiHoa04@users.noreply.github.com>
2025-08-10 23:19:13 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './pages';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
