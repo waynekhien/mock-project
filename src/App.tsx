9c9c3c7397899ff306951b59bbf733a1722f9610
LeThiHoa04 <129196168+LeThiHoa04@users.noreply.github.com>
2025-08-16 08:41:04 +0700

    App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,61 @@
+import React, { ReactNode } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import Header from "./components/Header";
+import Dashboard from "./pages/Dashboard";
+import OrdersAdmin from "./pages/OrdersAdmin";
+import ProductsAdmin from "./pages/ProductsAdmin";
+import UsersAdmin from "./pages/UsersAdmin";
+import Login from "./pages/Login";
+import useAuth from "./hooks/useAuth";
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Home } from './pages';
+interface AdminLayoutProps {
+  children: ReactNode;
+}
 
-function App() {
+const AdminLayout: React.FC<AdminLayoutProps> = ({ children }) => {
+  const { isLoggedIn } = useAuth();
+  return isLoggedIn ? (
+    <div className="flex bg-slate-100 min-h-screen font-sans">
+      <Sidebar />
+      <div className="flex-1 ml-64 flex flex-col">
+        <Header />
+        <main className="p-8 flex-1">{children}</main>
+      </div>
+    </div>
+  ) : (
+    <Navigate to="/login" />
+  );
+};
+
+const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/admin/*"
+          element={
+            <AdminLayout>
+              <Routes>
+                <Route path="dashboard" element={<Dashboard />} />
+                <Route path="products" element={<ProductsAdmin />} />
+                <Route path="users" element={<UsersAdmin />} />
+                <Route path="orders" element={<OrdersAdmin />} />
+                <Route path="*" element={<Navigate to="dashboard" />} />
+              </Routes>
+            </AdminLayout>
+          }
+        />
+        <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
     </Router>
   );
-}
+};
 
 export default App;
